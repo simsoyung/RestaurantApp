4f680c6633a097d337116fc30a9611631e023a1f
soyung sim <simsy1024@gmail.com>
2024-05-28 18:42:29 +0900

    Initial Commit

---
+++ RestaurantApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestaurantApp
+//
+//  Created by 심소영 on 5/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
